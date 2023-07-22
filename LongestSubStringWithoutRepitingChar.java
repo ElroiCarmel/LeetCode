0c88e665e02e3f8420adf133bd3206459a33f309
ELRO <elroica277@gmail.com>
2023-07-22 22:19:10 +0300

    Right solution with HashMap

--- LongestSubStringWithoutRepitingChar.java
+++ LongestSubStringWithoutRepitingChar.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class LongestSubStringWithoutRepitingChar {
 
@@ -5,19 +6,23 @@ public class LongestSubStringWithoutRepitingChar {
 		// TODO Auto-generated method stub
 		String test = "ahheelo";
 		System.out.println(solution(test));
+		String test2 = "abcabcbb";
+		System.out.println(solution(test2));
+
 	}
 	
 	public static int solution(String s) {
 		if (s.length()==1) return 1;
 		int ans=0;
-		int start = 0;
-		for (int e=1; e<s.length();e++) {
-			if (s.charAt(e-1)==s.charAt(e)) {
-				if (e-start>ans) ans = e-start;
-				start = e-1;
+		HashMap<Character, Integer> help = new HashMap();
+		for (int j=0; j<s.length();j++) {
+			if (help.get(s.charAt(j))==null) {
+				help.put(s.charAt(j), 1);
+			} else {
+				if (help.size()>ans) ans = help.size();
+				help = new HashMap<Character, Integer>();
 			}
 		}
-		if (s.length()-1-start>ans) ans = s.length()-1-start;
 		return ans;
 	}
 }
