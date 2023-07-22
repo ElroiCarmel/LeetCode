096a21d4c28209370605fe85d35f3ffa8d61ef99
ELRO <elroica277@gmail.com>
2023-07-22 22:46:41 +0300

    Update test3 not OK

--- LongestSubStringWithoutRepitingChar.java
+++ LongestSubStringWithoutRepitingChar.java
@@ -8,7 +8,10 @@ public class LongestSubStringWithoutRepitingChar {
 		System.out.println(solution(test));
 		String test2 = "pwwkew";
 		System.out.println(solution(test2));
-
+		String test3 = "dvdf";
+		System.out.println(solution(test3));
+		// solution not right for test3
+		
 	}
 	
 	public static int solution(String s) {
@@ -17,10 +20,10 @@ public class LongestSubStringWithoutRepitingChar {
 		HashMap<Character, Integer> help = new HashMap();
 		for (int j=0; j<s.length();j++) {
 			if (help.get(s.charAt(j))==null) {
-				help.put(s.charAt(j), 1);
+				help.put(s.charAt(j), j);
 			} else {
-				if (help.size()>ans) ans = help.size();
-				help = new HashMap<Character, Integer>();
+				if (j-help.get(s.charAt(j))>ans) ans = j-help.get(s.charAt(j));
+//				help = new HashMap<Character, Integer>();
 				help.put(s.charAt(j), 1);
 			}
 		}
