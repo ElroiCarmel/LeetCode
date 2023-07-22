e48a28804c7b1833aa66a334336e92f28428c431
ELRO <elroica277@gmail.com>
2023-07-22 22:05:19 +0300

    V1 solution O(n)

--- LongestSubStringWithoutRepitingChar.java
+++ LongestSubStringWithoutRepitingChar.java
@@ -3,7 +3,21 @@ public class LongestSubStringWithoutRepitingChar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Heloo");
+		String test = "ahheelo";
+		System.out.println(solution(test));
 	}
-
+	
+	public static int solution(String s) {
+		if (s.length()==1) return 1;
+		int ans=0;
+		int start = 0;
+		for (int e=1; e<s.length();e++) {
+			if (s.charAt(e-1)==s.charAt(e)) {
+				if (e-start>ans) ans = e-start;
+				start = e-1;
+			}
+		}
+		if (s.length()-1-start>ans) ans = s.length()-1-start;
+		return ans;
+	}
 }
